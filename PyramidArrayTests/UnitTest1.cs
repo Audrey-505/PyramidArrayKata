283e96f357107d9b1fcc1621b98f88559f22dc05
Audrey Alcantara <audreyalcantara@Audreys-MBP.lan>
2024-03-26 07:33:50 -0400

    test: adding tests

---
+++ PyramidArrayTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using PyramidArrayKata;
+
+namespace PyramidArrayTests
+{
+    public class PyramidArrayTests
+    {
+        [Fact]
+        public void CreateArray_ShouldReturn_ArrayWithLengthOfInput()
+        {
+            int input = 3;
+            int lengthOfArray = PyramidArray.CreateArray(input);
+            lengthOfArray.Should().Be(input);
+
+        }
+
+        [Fact]
+        public void CreateJaggedArray_ShouldReturn_JaggedArrayWithLengthOfInput()
+        {
+            int input = 3;
+            int lengthOfJaggedArray = PyramidArray.CreateJaggedArray(input);
+            lengthOfJaggedArray.Should().Be(input);
+        }
+
+        [Fact]
+        public void FullJaggedArray_ShouldReturn_JaggedArrayWithValues()
+        {
+            int input = 3;
+            int[][] jaggedArray = new int[input][];
+            jaggedArray[0] = new int[] {1};
+            jaggedArray[1] = new int[] {1,1};
+            jaggedArray[2] = new int[] {1,1,1};
+            int[][] fullJaggedArray = PyramidArray.FullJaggedArray(input);
+            fullJaggedArray.Should().BeEquivalentTo(jaggedArray);
+        }
+
+    }
+}
+
